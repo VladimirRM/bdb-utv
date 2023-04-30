393726eb64cb11f0da221d30d2fe0c7ae4d6bce1
Vladimir RM <shvdrummer@gmail.com>
2023-04-30 13:37:06 +0300

    Create schema

---
+++ Role.js
@@ -0,0 +1,8 @@
+const { Schema,model } = require("mongoose");
+
+const user = new Schema({
+    username: {type:String , unique: true , require: true},
+    username: {type:String , unique: true , require: true},
+    username: {type:String , unique: true , require: true},
+})
+
