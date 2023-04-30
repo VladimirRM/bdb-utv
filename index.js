ed1b4925c695dff57b7c99b696c1064360123b6f
Vladimir RM <shvdrummer@gmail.com>
2023-04-30 11:25:15 +0300

    authRoutes create

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const mongoose = require("mongoose")
 const PORT = process.env.PORT || 5000
 const app = express()
 
-const start =()=> {
+app.use(express.json())
+
+const start = async ()=> {
     try{
+        await mongoose.connect('mongodb+srv://user:user@cluster0.k5omo2x.mongodb.net/uldb?retryWrites=true&w=majority')
       app.listen(PORT, ()=>{
         console.log(`server started on port ${PORT}`)
       })
