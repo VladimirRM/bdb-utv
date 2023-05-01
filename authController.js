98d4b84961b31fed4dc327e4a50c19b753f15249
Vladimir RM <shvdrummer@gmail.com>
2023-05-01 14:49:16 +0300

    Work on validator

--- authController.js
+++ authController.js
@@ -1,61 +1,38 @@
-// const User = require('./models/User')
-// const Role = require('./models/Role')
 
-// class authController {
-//     async registration (req,res){
-//   try{
-    
-//   }catch  (e){
-    
-//   }
-//     }
-//     async login (req,res){
-// try{
-
-// }catch (e){
-
-// }
-        
-//     }
-//     async getUsers (req,res){
-//       try{
-//         const userRole = new Role()
-//         const adminRole = new Role({value:"ADMIN"})
-//          await userRole.save()
-//          await adminRole.save()
-//        res.json('server works')
-//       }catch  (e){
-
-//       }
-//     }
-// }
-
-
-// module.exports = new authController()
  const User = require('./models/User')
  const Role = require('./models/Role')
+ const dcrypt = require('dcryptjs');
+ const {validationResult} = require('express-validator')
 
 class authController {
     async registration(req,res){
       try{
-
+       const {username, password} = req.body
+       const candidate =  await User.findOne({username})
+       if( candidate){
+        return res.status(400).json({massage: 'Пользователь с таким именем уже существует'})
+       }
+       const hashPassword = dcrypt.hash("password",7);
+       const userRole = await Role.findOne({value: 'USER'})
+       const user = new User({username,password: hashPassword,roles:[userRole.value ]})
+       await user.save()
+       return res.json({ message:'Пользователь успешно зарегестрирован'})
       } catch(e){
-
+        console.log(e)
+        res.status(400).json({massage: 'Registration error'}) 
       } 
     }
     async login(req,res){
    try{
 
       } catch(e){
-        
+       console.log(e)
+       res.status(400).json({massage: 'Login error'}) 
       } 
     }
     async getUsers(req,res){
         try{
-             const userRole = new Role()
-             const adminRole = new Role({value: "ADMIN"})
-              await userRole.save()
-              await adminRole.save()
+          
          res.json('server work')
         } catch(e){
           
--- authRouter.js
+++ authRouter.js
@@ -11,8 +11,14 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration',controller.registration)
+router.post('/registration',
+[
+check("username","Имя пользователя не может быть пустым").notEmpty(),
+check("password","Пароль должен быть не менее 4 и не более 10 символов").isLength({min:4,max:10})
+]
+,controller.registration)
 router.post('/login',controller.login)
 router.get('/users',controller.getUsers)
 
