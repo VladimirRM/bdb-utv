f78d20cfccd3d7379f2dfd9f574b2fbefe554869
Vladimir RM <shvdrummer@gmail.com>
2023-05-01 15:20:55 +0300

    create  const generateAccessToken

--- authController.js
+++ authController.js
@@ -2,11 +2,20 @@
  const User = require('./models/User')
  const Role = require('./models/Role')
  const dcrypt = require('dcryptjs');
+ const jwt = require('jsonwebtoken');
  const {validationResult} = require('express-validator')
+
+ const generateAccessToken = () =>{
+    
+ }
 
 class authController {
     async registration(req,res){
       try{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json('Ошибка при регистрации' , errors)
+        }
        const {username, password} = req.body
        const candidate =  await User.findOne({username})
        if( candidate){
@@ -24,7 +33,16 @@ class authController {
     }
     async login(req,res){
    try{
-
+       const {username, password} = req.body
+       const user = await User.findOne({username})
+       if(!user){
+        return res.status(400).json({message:'Пользователь ${username} не найден'})
+       }
+       const validPassword = dcrypt.compareSync(password,user.password)
+       if(validPassword){
+        return res.status(400).json({message:'Введен неверный пароль'})
+        const token = generateAccessToken()
+       }
       } catch(e){
        console.log(e)
        res.status(400).json({massage: 'Login error'}) 
