c962bef1f0eef15d5b33d3c271ac9fde79425f5e
Vladimir RM <shvdrummer@gmail.com>
2023-05-02 09:05:50 +0300

    import authMiddleware

--- authRouter.js
+++ authRouter.js
@@ -2,6 +2,7 @@ const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
 const {check} = require('express-validator')
+const authMiddleware = require('./middleware/authMiddleware')
 
 router.post('/registration',
 [
