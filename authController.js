fedec60f703635ff4ed5cc3dee380a87df142178
Vladimir RM <shvdrummer@gmail.com>
2023-05-08 19:26:16 +0300

    Create authController

---
+++ authController.js
@@ -0,0 +1,26 @@
+class authController {
+    async registration (req,res){
+        try{
+
+        }catch(e){
+
+        }
+    }
+    async login(req,res){
+        try{
+
+        }catch(e){
+            
+        }
+
+    }
+    async getUsers(req,res){
+        try{
+
+        }catch(e){
+            
+        }
+    }
+}
+
+module.exports = new authController()
