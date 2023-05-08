f43851be880b592c77727e41ddb9f9cf448d3fc1
Vladimir RM <shvdrummer@gmail.com>
2023-05-08 21:53:38 +0300

    work with password hash

--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,13 @@
 const Router = require('express')
 const controller = require('./authController')
+const {check} = require('express-validator')
 
 const router = new Router()
 
-router.post('/registration',controller.registration)
+router.post('/registration',[
+    check('username','Имя пользователя не может быть пустым').notEmpty(),
+    check('password','Пароль не должен быть менее 4 и не более 10 символов').isLength({min:4,max:10})
+],controller.registration)
 router.post('/login',controller.login)
 
 router.get('/users',controller.getUsers)
