91e3a35be7187605831459c58475c055e22b43d0
Vladimir RM <shvdrummer@gmail.com>
2023-05-08 19:13:28 +0300

    connect mongodb

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 
-const start = ()=>{
+const start = async ()=>{
     try{
+        await mongoose.connect('mongodb+srv://user:user@cluster0.gwazgmm.mongodb.net/?retryWrites=true&w=majority')
      app.listen(PORT, ()=> console.log(`server started on port ${PORT}`))
     }catch(e){
         console.log(e)
